55d72174390b101b391402c3b237c370c34a04c4
Jose Sojo <jsojo346@gmail.com>
2022-07-22 17:24:24 -0400

    añadida /get

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get('/', function(req,res){
   res.send('All ok');
-})
+});
+
+router.get('/get', function(req,res){
+  res.send('get items');
+});
 
 module.exports = router;
