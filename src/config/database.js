f988456d1dd139c05c7a21dba929c94c2adf953d
Jose Sojo <jsojo346@gmail.com>
2022-07-22 19:27:37 -0400

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
 
 const local = 'mongodb://localhost/api-nodejs';
-const remote = 'mongodb+srv://demon258:j28*Sojo@cluster0.haig3y0.mongodb.net/node-mongodb-apirest?retryWrites=true&w=majority';
+const remote = 'mongodb://demon258:j28*Sojo@cluster0.haig3y0.mongodb.net/node-mongodb-apirest?retryWrites=true&w=majority';
 
 
 async function ConnectMongo(){
