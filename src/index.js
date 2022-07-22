b548f79d7f3f8180360b72a5cc3aa73ca8f8550f
Jose Sojo <jsojo346@gmail.com>
2022-07-22 17:32:31 -0400

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 7070);
+app.set('views', path.join(__dirname, '/views'));
+app.set('template', path.join(__dirname, '/views/template'));
+app.set('view engine', 'hbs')
 
 // routers
 app.use(require(path.join(__dirname, '/router')));
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req,res){
-  res.send('All ok');
+  //res.render('');
+  res.send('javier es marico');
 });
 
 router.get('/get', function(req,res){
